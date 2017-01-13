d1aca9a1bb96d79e5323fe20aa0a10ca10dee983
Gustavo Saiani <gustavo@poe.ma>
2017-01-13 17:10:07 -0200

    Start Player files

--- src/Main.elm
+++ src/Main.elm
@@ -1,41 +1,21 @@
 module Main exposing (..)
 
 import Html exposing (Html, div, text, program)
+import Messages exposing (Msg)
+import Models exposing (Model)
+import Update exposing (update)
+import View exposing (view)
 
--- MODEL
-
-type alias Model =
-  String
 
 init : ( Model, Cmd Msg )
 init =
   ( "Hello", Cmd.none )
 
--- MESSAGES
-
-type Msg
-  = NoOp
-
--- VIEW
-
-view : Model -> Html Msg
-view model =
-  div []
-    [ text model ]
-
--- UPDATE
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-  case msg of
-    NoOp ->
-      ( model, Cmd.none )
-
--- SUBSCRIPTIONS
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.none
+
 
 -- MAIN
 
